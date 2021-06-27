5769016abd9786b40eb3e346f927f101ec25b077
patelashok <ashok.patel2917@gmail.com>
2021-06-28 01:19:51 +0530

    solve using array

---
+++ Array/PrimeFector.js
@@ -0,0 +1,23 @@
+var userInputNum = 10;
+
+/**Resulting prime factors in an array */
+function primeFactors(number) {
+    const factors = [];
+    for (i = 2; i <= userInputNum; i++) {
+        if (userInputNum % i == 0) {
+            var flag = 0;
+            for (j = 2; j * j <= i; j++) {
+                if (i % j == 0) {
+                    flag = 1;
+                    break;
+                }
+            }
+            if (flag == 0) {
+                factors.push(i);
+            }
+        }
+    }
+    return factors
+}
+
+console.log("Prime factors for " + userInputNum + " are: " + primeFactors(userInputNum));
